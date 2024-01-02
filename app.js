dce4fa33d39a83b4eee098486a1abdfba50b9179
Naya252 <naya252naya@gmail.com>
2024-01-02 18:13:52 +0300

    refactor(no-rule): delete useless comments

--- app.js
+++ app.js
@@ -1,46 +1,32 @@
-//Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
-
 //Problem: User interaction does not provide the correct results.
 //Solution: Add interactivity so the user can manage daily tasks.
 //Break things down into smaller steps and take each step at a time.
 
+let taskInput = document.getElementById("new-task");
+let addButton = document.getElementsByTagName("button")[0];
+let todoTasks = document.getElementById("todo-tasks");
+let completedTasks = document.getElementById("completed-tasks");
 
-// Event handling, user interaction is what starts the code execution.
-
-let taskInput = document.getElementById("new-task");//Add a new task.
-let addButton = document.getElementsByTagName("button")[0];//first button
-let todoTasks = document.getElementById("todo-tasks");//ul of main__todo
-let completedTasks = document.getElementById("completed-tasks");//completed-tasks
-
-//New task list item
 const createNewTaskElement = function(taskString) {
   let listItem = document.createElement("li");
   listItem.className = "tasks__item";
 
-  //input (checkbox)
-  let checkBox = document.createElement("input");//checkbx
-  //label
-  let label = document.createElement("label");//label
-  //input (text)
-  let editInput = document.createElement("input");//text
-  //button.edit-btn
-  let editButton = document.createElement("button");//edit button
-
-  //button.delete-btn
-  let deleteButton = document.createElement("button");//delete button
-  let deleteButtonImg = document.createElement("img");//delete button image
+  let checkBox = document.createElement("input");
+  let label = document.createElement("label");
+  let editInput = document.createElement("input");
+  let editButton = document.createElement("button");
+  let deleteButton = document.createElement("button");
+  let deleteButtonImg = document.createElement("img");
 
   label.innerText = taskString;
   label.className = "tasks__item_label";
 
-  //Each elements, needs appending
   checkBox.type = "checkbox";
   checkBox.className = "tasks__item_checkbox";
   editInput.type = "text";
   editInput.className = "tasks__item_input";
 
-  editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
+  editButton.innerText = "Edit";
   editButton.className = "plain-btn edit-btn";
 
   deleteButton.className = "delete-btn";
@@ -49,29 +35,27 @@ const createNewTaskElement = function(taskString) {
   deleteButtonImg.alt = "remove";
   deleteButton.appendChild(deleteButtonImg);
 
-  //and appending.
   listItem.appendChild(checkBox);
   listItem.appendChild(label);
   listItem.appendChild(editInput);
   listItem.appendChild(editButton);
   listItem.appendChild(deleteButton);
+
   return listItem;
 }
 
 const addTask = function() {
   console.log("Add Task...");
-  //Create a new list item with the text from the #new-task:
+
   if (!taskInput.value) return;
   let listItem = createNewTaskElement(taskInput.value);
 
-  //Append listItem to todoTasks
   todoTasks.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
 
   taskInput.value = "";
 }
 
-//Edit an existing task.
 const editTask = function() {
   console.log("Edit Task...");
   console.log("Change 'edit' to 'save'");
@@ -97,8 +81,6 @@ const editTask = function() {
   listItem.classList.toggle("edit-mode");
 };
 
-
-//Delete task.
 const deleteTask = function () {
   console.log("Delete Task...");
 
@@ -108,11 +90,9 @@ const deleteTask = function () {
   ul.removeChild(listItem);
 }
 
-//Mark task completed
 const taskCompleted = function() {
   console.log("Complete Task...");
 
-  //Append the task list item to the #completed-tasks
   let listItem = this.parentNode;
   completedTasks.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
@@ -120,7 +100,7 @@ const taskCompleted = function() {
 
 const taskIncomplete = function () {
   console.log("Incomplete Task...");
-//Mark task as incomplete.
+
   //When the checkbox is unchecked
   //Append the task list item to the main__todo.
   let listItem = this.parentNode;
@@ -131,8 +111,6 @@ const taskIncomplete = function () {
 const ajaxRequest = function() {
   console.log("AJAX Request");
 }
-
-//The glue to hold it all together.
 
 //Set the click handler to the addTask function.
 addButton.onclick = addTask;
@@ -166,8 +144,6 @@ for (let i = 0; i < completedTasks.children.length; i++){
   //bind events to list items chldren(tasksIncompleted)
   bindTaskEvents(completedTasks.children[i], taskIncomplete);
 }
-
-// Issues with usability don't get seen until they are in front of a human tester.
 
 //prevent creation of empty tasks.
 
